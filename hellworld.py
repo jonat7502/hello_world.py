e16bad9ab2eab386de72a93ef4b59606474663bf
jonathan <jonat7502@yahoo.com>
2016-10-05 16:47:52 +0100

    Initil Commit

--- hellworld.py
+++ hellworld.py
@@ -1,1 +1,10 @@
-print "Hello, World"
+name = raw_input("Hi, what is your name?: ")
+
+raw_input("Hello Jonathan, would you like to play a game?:(y/n): ")
+
+raw_input("Good choice, the only way to win is not to play:(y): ")
+
+raw_input("You die in a horrible global thermonuclear war")
+
+
+
